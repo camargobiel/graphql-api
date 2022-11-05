3029138aa65fd22a7afd9b7710e8b167ca34aa46
camargobiel <camargoobiel@gmail.com>
2022-11-05 12:00:00 -0300

    feat: server with graphql and express

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const { loadSchemaSync } = require('@graphql-tools/load')
 const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader')
-const { ApolloServer } = require('@apollo/server')
-const { startStandaloneServer } = require('@apollo/server/standalone')
+
+const { ApolloServer } = require('apollo-server-express')
+
 const resolvers = require('./src/resolvers/characters.js')
+const express = require('express')
 
 const typeDefs = loadSchemaSync('./src/schemas/characters.graphql', {
   loaders: [new GraphQLFileLoader()]
 })
+
+const PORT = 3000
 
 const startServer = async () => {
   const server = new ApolloServer({
@@ -14,11 +18,23 @@ const startServer = async () => {
     resolvers
   })
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 3000 }
+  const app = express()
+
+  await server.start()
+
+  server.applyMiddleware({ app })
+
+  app.get('/rest', (req, res) => {
+    res.json({
+      data: 'API is working...'
+    })
   })
 
-  console.log(`🚀  Server ready at: ${url}`)
+  app.listen(PORT, () => {
+    console.log(`🚀  Server ready at: ${PORT}`)
+  })
 }
 
 startServer()
+
+/* module.exports = { server, typeDefs } */
