8904cef15b99259bbe5f6eeb19ab83488e0be62a
camargobiel <camargoobiel@gmail.com>
2022-11-07 15:51:05 -0300

    test: added id search case test

--- tests/characters.spec.js
+++ tests/characters.spec.js
@@ -19,23 +19,40 @@ describe('Queries', () => {
       expect(response.body.data.characters.length).toBe(20)
     })
 
-    /* it('given id should return specific character', async () => {
+    it('given id should return specific character', async () => {
       const response = await request(app)
         .post('/graphql')
         .send({
           query: `{
-            characters {
-              id
+            character(id: ${1}) {
+              name
             }
-          }`,
-          variables: {
-            id: 1
-          }
+          }`
         })
         .set('Accept', 'application/json')
         .expect(200)
 
-      console.log(response.data)
-    }) */
+      expect(response.body.data.character).toStrictEqual({
+        name: 'Rick Sanchez'
+      })
+    })
+  })
+
+  describe('Error', () => {
+    it('given invalid id should return error', async () => {
+      const response = await request(app)
+        .post('/graphql')
+        .send({
+          query: `{
+            character(id: ${999}) {
+              name
+            }
+          }`
+        })
+        .set('Accept', 'application/json')
+        .expect(200)
+
+      expect(response.body.data.character).toStrictEqual(null)
+    })
   })
 })
